65b594c3dc2f041bd01829feb24af38523f71bb8
minjujung-0530 <mj.jung@teamsparta.co>
2024-02-28 12:48:05 +0900

    feat: pomordoros ui 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/screens/home_screen.dart';
 
-// 많은 widget을 블럭 처럼 사용할 수 있음
-// 여러 widget을 합쳐서 ui 를 만들어 내는 것
-// widget 만드는 것 = class 만드는 것
 void main() {
   runApp(const App());
 }
