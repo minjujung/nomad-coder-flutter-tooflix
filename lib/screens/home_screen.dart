7a41dde5027681feffda2f60a066d5eb93fdd99f
minjujung-0530 <mj.jung@teamsparta.co>
2024-03-06 00:06:23 +0900

    feat: 타이머 기능 기본적인것 추가 완료

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 // st 하고 tab 누르면 stateless widget 만들어짐
@@ -8,20 +10,36 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
+class _HomeScreenState extends State<HomeScreen> {
+  int totalSeconds = 1500;
+  bool isRunning = false;
 
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(vsync: this);
+  // Dart 표준 라이브러리에 Timer 있음
+  // Timer 를 통해 정해진 간격에 한번씩 함수 실행 가능
+  late Timer timer;
+  // 사용자가 버튼을 누를때만 이 타이머 생성하도록 => late modifier 사용
+  // late modifier 는 이 property 를 당장 초기화 하지 않아도 된다는 것을 의미
+  // 하지만 property 를 사용하지 전에는 반드시 초기화 한다는 약속을 뜻하기도 함
+
+  void onTick(Timer timer) {
+    setState(() {
+      totalSeconds = totalSeconds - 1;
+    });
   }
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+  void onStartPressed() {
+    // 1초마다 onTick 함수를 호출
+    timer = Timer.periodic(const Duration(seconds: 1), onTick);
+    setState(() {
+      isRunning = true;
+    });
+  }
+
+  void onPausePressed() {
+    timer.cancel();
+    setState(() {
+      isRunning = false;
+    });
   }
 
   @override
@@ -36,7 +54,7 @@ class _HomeScreenState extends State<HomeScreen>
               child: Container(
                 alignment: Alignment.bottomCenter,
                 child: Text(
-                  '25:00',
+                  '$totalSeconds',
                   style: TextStyle(
                     color: Theme.of(context).cardColor,
                     fontSize: 89,
@@ -50,8 +68,11 @@ class _HomeScreenState extends State<HomeScreen>
               child: Center(
                 child: IconButton(
                   iconSize: 120,
-                  onPressed: null,
-                  icon: Icon(Icons.play_circle_outlined,
+                  onPressed: isRunning ? onPausePressed : onStartPressed,
+                  icon: Icon(
+                      isRunning
+                          ? Icons.pause_circle_outlined
+                          : Icons.play_circle_outlined,
                       color: Theme.of(context).cardColor),
                 ),
               ),
@@ -64,8 +85,8 @@ class _HomeScreenState extends State<HomeScreen>
                     // 꽉 채워버리기~
                     child: Container(
                       decoration: BoxDecoration(
-                        color: Theme.of(context).cardColor,
-                      ),
+                          color: Theme.of(context).cardColor,
+                          borderRadius: BorderRadius.circular(50)),
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
@@ -88,7 +109,7 @@ class _HomeScreenState extends State<HomeScreen>
                                     .textTheme
                                     .displayLarge!
                                     .color),
-                          )
+                          ),
                         ],
                       ),
                     ),
